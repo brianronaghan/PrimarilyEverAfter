0add9c1f98bfd82dbd0bbc1d59f7541c946e6c41
Brian Ronaghan <brian.ronaghan@gmail.com>
2016-02-16 18:11:02 -0800

    provides search functionality for polls inside the specific candidate

--- server/candidates/candidateController.js
+++ server/candidates/candidateController.js
@@ -11,13 +11,95 @@ var createCandidate = Q.nbind(Candidate.create, Candidate);
 var findAllCandidates = Q.nbind(Candidate.find, Candidate);
 var pollster = require('pollster');
 
+var cands = [{name: "Clinton",
+  fullName: 'Hillary Clinton',
+  party: 'dem',
+  image: '../assets/clinton.jpg',
+  delegateCount: 44,
+  money: 123456789,
+  url: "https://www.hillaryclinton.com/",
+  twitter: 'https://twitter.com/HillaryClinton'
+},
+{name: "Sanders",
+  fullName: 'Bernie Sanders',
+  party: 'dem',
+  image: '../assets/sanders.jpg',
+  delegateCount: 36,
+  money: 123456789,
+  url: 'http://www.berniesanders.com',
+  twitter: 'http://twitter.com/SenSanders'
+},
+{name: "Trump",
+  fullName: 'Donald Trump',
+  party: 'rep',
+  image: '../assets/trump.jpg',
+  delegateCount: 17,
+  money: 123456789,
+  url: 'https://www.donaldjtrump.com/',
+  twitter: 'http://twitter.com/realDonaldTrump'
+},
+{name: "Cruz",
+  fullName: "Ted Cruz",
+  party: 'rep',
+  image: '../assets/cruz.jpg',
+  delegateCount: 11,
+  money: 123456789,
+  url: "https://www.tedcruz.org/",
+  twitter: 'http://twitter.com/tedcruz'
+},
+{name: "Rubio",
+  fullName: 'Marco Rubio',
+  party: 'rep',
+  image: '../assets/rubio.jpg',
+  delegateCount: 10,
+  money: 123456789,
+  url: 'https://marcorubio.com/',
+  twitter: 'http://twitter.com/marcorubio'
+},
+{name: "Kasich",
+  fullName: "John Kasich",
+  party: 'rep',
+  image: '../assets/kasich.jpg',
+  delegateCount: 5,
+  money: 123456789,
+  url: 'http://johnkasich.com/',
+  twitter: 'http://twitter.com/JohnKasich'
+},
+{name: "Bush",
+  fullName: "Jeb Bush",
+  party: 'rep',
+  image: '../assets/bush.jpg',
+  delegateCount: 4,
+  money: 123456789,
+  url: 'https://jeb2016.com/?lang=en',
+  twitter: 'http://twitter.com/JebBush'
+},
+{name: "Carson",
+  fullName: "Ben Carson",
+  party: 'rep',
+  image: '../assets/carson.png',
+  delegateCount: 3,
+  money: 123456789,
+  url: 'https://www.bencarson.com/',
+  twitter: 'http://twitter.com/RealBenCarson'
+},
+{name: "Christie",
+  fullName: "Chris Christie",
+  party: 'rep',
+  image: '../assets/christie.jpg',
+  delegateCount: 0,
+  money: 123456789,
+  url: 'https://www.chrischristie.com/',
+  twitter: 'https://twitter.com/ChrisChristie'
+}];
+
 module.exports = {
   addCandidates: function (req, res, next) {
     console.log("whoohoo add cands");
 
-    // for(var x = 0; x < cands.length; x++) {
-    //   createCandidate(cands[x]);
-    // }
+    for(var x = 0; x < cands.length; x++) {
+      createCandidate(cands[x]);
+    }
   },
   allCandidates: function (req, res, next) {
     findAllCandidates({})
@@ -87,84 +169,3 @@ module.exports = {
 // INITIAL DB UPLOAD:
 
 //
-// var cands = [{name: "Clinton",
-//   fullName: 'Hilary Clinton',
-//   party: 'dem',
-//   image: '../assets/clinton.jpg',
-//   delegateCount: 44,
-//   money: 123456789,
-//   url: "https://www.hillaryclinton.com/",
-//   twitter: 'https://twitter.com/HillaryClinton'
-// },
-// {name: "Sanders",
-//   fullName: 'Bernie Sanders',
-//   party: 'dem',
-//   image: '../assets/sanders.jpg',
-//   delegateCount: 36,
-//   money: 123456789,
-//   url: 'http://www.berniesanders.com',
-//   twitter: 'http://twitter.com/SenSanders'
-// },
-// {name: "Trump",
-//   fullName: 'Donald Trump',
-//   party: 'rep',
-//   image: '../assets/trump.jpg',
-//   delegateCount: 17,
-//   money: 123456789,
-//   url: 'https://www.donaldjtrump.com/',
-//   twitter: 'http://twitter.com/realDonaldTrump'
-// },
-// {name: "Cruz",
-//   fullName: "Ted Cruz",
-//   party: 'rep',
-//   image: '../assets/cruz.jpg',
-//   delegateCount: 11,
-//   money: 123456789,
-//   url: "https://www.tedcruz.org/",
-//   twitter: 'http://twitter.com/tedcruz'
-// },
-// {name: "Rubio",
-//   fullName: 'Marco Rubio',
-//   party: 'rep',
-//   image: '../assets/rubio.jpg',
-//   delegateCount: 10,
-//   money: 123456789,
-//   url: 'https://marcorubio.com/',
-//   twitter: 'http://twitter.com/marcorubio'
-// },
-// {name: "Kasich",
-//   fullName: "John Kasich",
-//   party: 'rep',
-//   image: '../assets/kasich.jpg',
-//   delegateCount: 5,
-//   money: 123456789,
-//   url: 'http://johnkasich.com/',
-//   twitter: 'http://twitter.com/JohnKasich'
-// },
-// {name: "Bush",
-//   fullName: "Jeb Bush",
-//   party: 'rep',
-//   image: '../assets/bush.jpg',
-//   delegateCount: 4,
-//   money: 123456789,
-//   url: 'https://jeb2016.com/?lang=en',
-//   twitter: 'http://twitter.com/JebBush'
-// },
-// {name: "Carson",
-//   fullName: "Ben Carson",
-//   party: 'rep',
-//   image: '../assets/carson.png',
-//   delegateCount: 3,
-//   money: 123456789,
-//   url: 'https://www.bencarson.com/',
-//   twitter: 'http://twitter.com/RealBenCarson'
-// },
-// {name: "Christie",
-//   fullName: "Chris Christie",
-//   party: 'rep',
-//   image: '../assets/christie.jpg',
-//   delegateCount: 0,
-//   money: 123456789,
-//   url: 'https://www.chrischristie.com/',
-//   twitter: 'https://twitter.com/ChrisChristie'
-// }];
